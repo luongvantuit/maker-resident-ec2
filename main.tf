e3b89cd629f64206c30e1b5c3958c9f77f2de7b2
Lương Văn Tú <luongvantuit@gmail.com>
2022-05-30 00:28:17 +0700

    veriable terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+resource "aws_instance" "instance" {
+  instance_type = var.aws_ec2_instance_type
+}
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "aws_ec2_instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "Instance type of AWS EC2 instance"
+}
