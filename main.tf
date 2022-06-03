ed7f0aae6c19f15fe4678171a25128e8b3100d79
Lương Văn Tú <luongvantuit@gmail.com>
2022-06-03 18:06:31 +0700

    ec2 instances c6g medium arm64

--- main.tf
+++ main.tf
@@ -1,7 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
 provider "aws" {
-  region = "ap-southeast-1"
+  profile = "default"
+  region  = "ap-southeast-1"
 }
 
 resource "aws_instance" "instance" {
+
+  ami = var.aws_ec2_instance_ami
+
   instance_type = var.aws_ec2_instance_type
+
+  key_name = var.aws_ec2_instance_key_name
+  tags = {
+    "Name" = "${var.aws_ec2_instance_tag_name}"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,21 @@
 variable "aws_ec2_instance_type" {
   type        = string
-  default     = "t2.micro"
+  default     = "c6g.medium"
   description = "Instance type of AWS EC2 instance"
+}
+
+variable "aws_ec2_instance_tag_name" {
+  type        = string
+  default     = "DEFAULT"
+  description = "Name of the EC2 instance"
+}
+
+variable "aws_ec2_instance_ami" {
+  type    = string
+  default = "ami-07b575563ed0b0d0c"
+}
+
+variable "aws_ec2_instance_key_name" {
+  type    = string
+  default = "ec2__of__tu"
 }
